4e57aae8d498dff1b8ebdef2617f711f5733f5fe
thaiebu785 <mohamedthaiebu@Mohameds-MacBook-Pro.local>
2026-04-26 19:58:28 +0530

    update github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 export default defineConfig({
+  base: '/River-Frontend/',
   plugins: [react()],
   server: {
     port: 3000
